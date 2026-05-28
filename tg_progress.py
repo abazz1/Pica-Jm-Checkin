49c140e6a86bacb168eeaa3024fbc2c4abe17b99
root <root@localhost.localdomain>
2026-05-28 12:58:20 +0000

    Fix: graceful TG error handling in tg_progress.py

--- tg_progress.py
+++ tg_progress.py
@@ -10,7 +10,10 @@ def tg_progress(step, total, text):
     bar = '█' * (pct // 5) + '░' * (20 - pct // 5)
     msg = f'<b>每日签到</b>\n\n进度 |{bar}| {pct}%\n[{step}/{total}] {text}'
     data = json.dumps({'chat_id': int(ci), 'message_id': int(mi), 'text': msg, 'parse_mode': 'HTML'}).encode()
-    urllib.request.urlopen(f'https://api.telegram.org/bot{bt}/editMessageText', data)
+    try:
+        urllib.request.urlopen(f'https://api.telegram.org/bot{bt}/editMessageText', data)
+    except:
+        pass
 
 if __name__ == '__main__':
     tg_progress(int(sys.argv[1]), int(sys.argv[2]), sys.argv[3])
